923555c5343a7fd68ae755c83abd715f0ff2bb17
Zach Pomerantz <zzmp@uniswap.org>
2022-05-26 12:13:18 -0700

    fix: isolate hre and vary ports (#9)
    * fix: isolate hre
    
    * fix: vary port to allow multiple hre instances
    
    * fix: multi-worker e2e tests
    
    * chore: test job title
    
    * fix: use jest v27 types
    
    * fix: configure max workers

--- src/setup.ts
+++ src/setup.ts
@@ -7,33 +7,46 @@ import { HardhatNetworkAccountsConfig, JsonRpcServer } from 'hardhat/types'
 import { toExternallyOwnedAccounts } from './accounts'
 import { Hardhat } from './hardhat'
 
-if (!hre.config.networks.hardhat.forking) {
-  throw new Error(
-    '`forking` must be specified to use jest-environment-hardhat.\nSee https://hardhat.org/hardhat-network/guides/mainnet-forking.html#mainnet-forking.'
-  )
-}
-
-// Override the GET_PROVIDER task to avoid unnecessary time-intensive evm calls.
-hre.tasks[TASK_NODE_GET_PROVIDER].setAction(async () => hre.network.provider)
-
-const wallets = toExternallyOwnedAccounts(hre.network.config.accounts as HardhatNetworkAccountsConfig)
-if (wallets.length > 4) {
-  process.stderr.write(`${wallets.length} hardhat accounts specified - consider specifying fewer.\n`)
-  process.stderr.write('Specifying multiple hardhat accounts will noticeably slow your test startup time.\n\n')
-}
-
-globalThis.hardhat = new Hardhat(hre.config.networks.localhost.url, wallets)
-
 export default async function setup(): Promise<() => Promise<void>> {
-  hre.run(TASK_NODE)
-  const server = await new Promise<JsonRpcServer>((resolve) =>
-    hre.tasks[TASK_NODE_SERVER_READY].setAction(async ({ server }) => resolve(server))
+  if (!hre.config.networks.hardhat.forking) {
+    throw new Error(
+      '`forking` must be specified to use jest-environment-hardhat.\nSee https://hardhat.org/hardhat-network/guides/mainnet-forking.html#mainnet-forking.'
+    )
+  }
+  if (!hre.ethers) {
+    throw new Error(
+      'jest-environment-hardhat requires the hardhat-ethers plugin to be installed.\nSee https://hardhat.org/plugins/nomiclabs-hardhat-ethers.html#hardhat-ethers.'
+    )
+  }
+
+  // Override the GET_PROVIDER task to avoid unnecessary time-intensive evm calls.
+  hre.tasks[TASK_NODE_GET_PROVIDER].setAction(async () => hre.network.provider)
+  const port = 8545 + Number(process.env.JEST_WORKER_ID || 1)
+  const run = hre.run(TASK_NODE, { port })
+  const serverReady = new Promise<{ url: string; server: JsonRpcServer }>((resolve) =>
+    hre.tasks[TASK_NODE_SERVER_READY].setAction(async ({ address, port, server }) => {
+      const url = 'http://' + address + ':' + port
+      resolve({ url, server })
+    })
   )
+
+  // Deriving ExternallyOwnedAccounts is computationally intensive, so it is done while waiting for the server to come up.
+  const wallets = toExternallyOwnedAccounts(hre.network.config.accounts as HardhatNetworkAccountsConfig)
+  if (wallets.length > 4) {
+    process.stderr.write(`${wallets.length} hardhat accounts specified - consider specifying fewer.\n`)
+    process.stderr.write('Specifying multiple hardhat accounts will noticeably slow your test startup time.\n\n')
+  }
+
+  const { url, server } = await serverReady
+  globalThis.hardhat = new Hardhat(hre, url, wallets)
 
   // Enables hardhat logging if --verbose was passed.
   if (hre.config.networks.hardhat.loggingEnabled || process.argv.includes('--verbose')) {
     await hre.network.provider.send('hardhat_setLoggingEnabled', [true])
   }
 
-  return async () => await server.close()
+  return async () => {
+    await server.close()
+    await run
+  }
 }
