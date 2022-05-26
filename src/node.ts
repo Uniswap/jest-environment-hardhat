080fc136e48c03d22d1549f07160d285245183d6
Zach Pomerantz <zzmp@uniswap.org>
2022-05-25 17:41:03 -0700

    style: lint

--- src/node.ts
+++ src/node.ts
@@ -1,23 +1,24 @@
 import NodeEnvironment from 'jest-environment-node'
+
 import { setup, teardown } from './'
 
 export default class HardhatNodeEnvironment extends NodeEnvironment {
   constructor(config, context) {
-    super(config, context);
+    super(config, context)
   }
 
   async setup() {
-    await super.setup();
+    await super.setup()
     await setup()
     this.global.hardhat = hardhat
   }
 
   async teardown() {
-    await teardown();
-    await super.teardown();
+    await teardown()
+    await super.teardown()
   }
 
   getVmContext() {
-    return super.getVmContext();
+    return super.getVmContext()
   }
 }
