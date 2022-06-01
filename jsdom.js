80542103399d29d93acfa3ad3b3c322b138fbecf
Lint Action <lint-action@uniswap.org>
2022-06-01 15:24:10 +0000

    style(lint): lint action with ESLint

--- jsdom.js
+++ jsdom.js
@@ -4,11 +4,18 @@
  * It allows the environment to be specified as `hardhat/jsdom`, omitting the `dist/` directory.
  */
 
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = void 0;
-var jsdom_1 = require("./dist/jsdom");
-Object.defineProperty(exports, "default", { enumerable: true, get: function () { return __importDefault(jsdom_1).default; } });
+'use strict'
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod }
+  }
+Object.defineProperty(exports, '__esModule', { value: true })
+exports.default = void 0
+var jsdom_1 = require('./dist/jsdom')
+Object.defineProperty(exports, 'default', {
+  enumerable: true,
+  get() {
+    return __importDefault(jsdom_1).default
+  },
+})
--- node.js
+++ node.js
@@ -4,11 +4,18 @@
  * It allows the environment to be specified as `hardhat/node`, omitting the `dist/` directory.
  */
 
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = void 0;
-var node_1 = require("./dist/node");
-Object.defineProperty(exports, "default", { enumerable: true, get: function () { return __importDefault(node_1).default; } });
+'use strict'
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod }
+  }
+Object.defineProperty(exports, '__esModule', { value: true })
+exports.default = void 0
+var node_1 = require('./dist/node')
+Object.defineProperty(exports, 'default', {
+  enumerable: true,
+  get() {
+    return __importDefault(node_1).default
+  },
+})
